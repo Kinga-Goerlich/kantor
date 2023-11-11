92f42c6dfb1274a1ec0bc48c635d96d55d428ffb
Kinga Goerlich <kingagorlich@gmail.com>
2023-11-11 15:42:25 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+let rateEUR = 4.57;
+let rateCZK = 0.19;
+let rateUSD = 4.33;
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+
+        case "CZK":
+            result = amount / rateCZK;
+            break
+
+        case "USD":
+            result = amount / rateUSD;
+            break
+    }
+
+    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
+});
+
+
